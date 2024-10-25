cad7ded4ebac1476a1fbfeafc8f864099df2c707
Esteban Padilla <estebanpadilla469@gmail.com>
2024-10-25 15:13:45 -0600

    Se agrega matriz

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lexico_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (Lexico lexico = new Lexico())
+                {
+                    while (!lexico.finArchivo())
+                    {
+                        lexico.nextToken();
+                    }
+                    lexico.log.WriteLine("\n-----------------------------------\n");
+                    lexico.log.WriteLine("Líneas del archivo: " + lexico.linea);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
